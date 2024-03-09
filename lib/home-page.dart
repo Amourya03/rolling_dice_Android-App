4fd958a78026076d9394405a9b3a2d6ecf35e61c
Akshay Kumar Mourya <akshaymouryaart@gmail.com>
2024-03-09 15:13:51 +0530

    Initial commit

---
+++ lib/home-page.dart
@@ -0,0 +1,105 @@
+
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> with SingleTickerProviderStateMixin {
+  int leftDiceNumber=1;
+  int rightDiceNumber=1;
+  late AnimationController _controller;
+  late CurvedAnimation animation;
+
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    animate();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    _controller.dispose();
+  }
+
+  animate()
+  {
+    _controller =
+        AnimationController(vsync: this, duration: Duration(seconds: 1));
+    animation = CurvedAnimation(parent: _controller, curve: Curves.bounceOut);
+    animation.addListener(() {
+      setState(() {
+
+      });
+      print(_controller.value);});
+    animation.addStatusListener((status) {
+      if(status == AnimationStatus.completed){
+        setState(() {
+          leftDiceNumber=Random().nextInt(6)+1;
+          rightDiceNumber=Random().nextInt(6)+1;
+        });
+        print('Completed');
+        _controller.reverse();
+      }
+    });
+  }
+
+  void roll(){
+    _controller.forward();
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Dicee'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Row(
+                children: [
+                  Expanded(
+                      child: GestureDetector(
+                          onDoubleTap: roll,
+                          child: Padding(
+                            padding: EdgeInsets.all(15),
+                            child: Image(height: 200 - (_controller.value)*200,
+                                image: AssetImage('assets/images/dice-png-$leftDiceNumber.png')
+                            ),
+                          )
+                      )
+                  ),
+                  Expanded(
+                      child: GestureDetector(
+                          onDoubleTap: roll,
+                          child: Padding(
+                            padding: EdgeInsets.all(15),
+                            child: Image(height: 200 - (_controller.value)*200,
+                                image: AssetImage('assets/images/dice-png-$rightDiceNumber.png'),
+                            ),
+                          )
+                      )
+                  )
+                ],
+              ),
+              ElevatedButton(onPressed: roll,
+                child: Text('Roll', style: TextStyle(fontWeight: FontWeight.bold),),)],
+          ),
+        )
+    );
+  }
+}
